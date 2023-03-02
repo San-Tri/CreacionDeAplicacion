20fdaac21a13cd59b37e7cab61f841aa58ded040
San-Tri <1473024@senati.pe>
2023-03-02 15:39:39 -0500

    FerreteriElTornilloFeliz.v.3

--- ferreteriaElTornilloFeliz.py
+++ ferreteriaElTornilloFeliz.py
@@ -7,7 +7,11 @@ class Product:
     def __init__(self, window):
         self.wind = window
         self.wind.title('Products application')
+        # Crear un frame dentro de contenedor
+        frame = LabelFrame(self.wind, text='Registre un nuevo producto.')
+        frame.grid(row=0, column=0, columnspan=3, pady=20)
         
+        #     
 if __name__== '__main__' :
     window = Tk()
     application = Product(window)
